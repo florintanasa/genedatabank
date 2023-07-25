d98d87de63b9ecacfa54457d13faf5447b128d26
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-07-25 18:30:12 +0300

    First commit

---
+++ src/main/java/com/genebank/genedatabank/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.genebank.genedatabank.view.user;
+
+import com.genebank.genedatabank.entity.User;
+import com.genebank.genedatabank.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
