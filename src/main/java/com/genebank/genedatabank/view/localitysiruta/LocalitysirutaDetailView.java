f0bb1c9fca853c7f2a903f2825ec21632baa839a
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-07-27 17:40:35 +0300

    feat: Try to initialize the map

--- src/main/java/com/genebank/genedatabank/view/localitysiruta/LocalitysirutaDetailView.java
+++ src/main/java/com/genebank/genedatabank/view/localitysiruta/LocalitysirutaDetailView.java
@@ -4,12 +4,37 @@ import com.genebank.genedatabank.entity.Localitysiruta;
 
 import com.genebank.genedatabank.view.main.MainView;
 
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
 import io.jmix.flowui.view.*;
+import software.xdev.vaadin.maps.leaflet.flow.LMap;
+import software.xdev.vaadin.maps.leaflet.flow.data.LCenter;
 
 @Route(value = "localitysirutas/:id", layout = MainView.class)
 @ViewController("Localitysiruta.detail")
 @ViewDescriptor("localitysiruta-detail-view.xml")
 @EditedEntityContainer("localitysirutaDc")
 public class LocalitysirutaDetailView extends StandardDetailView<Localitysiruta> {
+    @ViewComponent
+    private VerticalLayout mapContainer;
+
+    private static final double DEFAULT_LATITUDE = 46.009628;
+    private  static final double DEFAULT_LONGITUDE = 24.456255;
+    private static final int ZOOM_LEVEL =7;
+
+    private LMap map = new LMap();
+
+    @Subscribe
+    protected void onInit(InitEvent event) {
+        initMap();
+     //   addMapToContainer();
+    }
+
+    private void initMap() {
+        map = new LMap();
+        map.setZoom(ZOOM_LEVEL);
+        map.setCenter(new LCenter(DEFAULT_LATITUDE, DEFAULT_LONGITUDE));
+    }
+
 }
