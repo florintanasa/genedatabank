fc2c48aca150efe3205a212cc7c9b315464fbce9
Florin Tanasa <florin.tanasa@gmail.com>
2023-08-03 11:34:05 +0300

    feat: Added sql data for zonesiruta table

---
+++ src/main/resources/com/genebank/genedatabank/liquibase/changelog/genedatabank_zonesiruta.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.zonesiruta (id,"version",created_by,created_date,last_modified_by,last_modified_date,deleted_by,deleted_date,siruta,zonename,zonenumber) VALUES
+	 ('aa9c3ae7-5fd5-4844-b7fb-27d42b8e74fc',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,813,'Nord-Est',1),
+	 ('f8bd97c7-af0c-44ae-a138-3874427a981b',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,822,'Sud-Est',2),
+	 ('2619c2aa-23d8-454b-aaf1-8e0d7ef20e27',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,831,'Sud-Muntenia',3),
+	 ('9fe21079-77f8-4794-9864-9fa288d6fd60',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,840,'Sud-Vest-Oltenia',4),
+	 ('8a76ebc5-5fc9-423f-aa49-c35053bd1f70',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,859,'Vest',5),
+	 ('b2f0acd7-6b3f-48f4-b941-99b3a7a30e39',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,868,'Nord-Vest',6),
+	 ('d75298c1-a829-45c1-9205-cbee3af30120',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,877,'Centru',7),
+	 ('fe740d20-bb75-4eb4-b025-721b0722a413',1,'admin','2021-01-27 14:10:16',NULL,'2021-01-27 14:10:16',NULL,NULL,886,'București-Ilfov',8);
