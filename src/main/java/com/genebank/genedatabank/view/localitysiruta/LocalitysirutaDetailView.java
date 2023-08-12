188e189b8bc053f812b5830ba0df7e3b8bd4a44a
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-08-12 17:27:29 +0300

    feat: Added possibility to work with OpenStreetMap and Google Maps

--- src/main/java/com/genebank/genedatabank/view/localitysiruta/LocalitysirutaDetailView.java
+++ src/main/java/com/genebank/genedatabank/view/localitysiruta/LocalitysirutaDetailView.java
@@ -14,6 +14,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.checkbox.JmixCheckbox;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import software.xdev.vaadin.maps.leaflet.flow.LMap;
@@ -26,6 +27,12 @@ import software.xdev.vaadin.maps.leaflet.flow.data.LTileLayer;
 @ViewDescriptor("localitysiruta-detail-view.xml")
 @EditedEntityContainer("localitysirutaDc")
 public class LocalitysirutaDetailView extends StandardDetailView<Localitysiruta> {
+    @Autowired
+    private MessageBundle messageBundle;
+    @Autowired
+    private Notifications notifications;
+    @ViewComponent
+    private JmixCheckbox checkbox;
     @ViewComponent
     private VerticalLayout mapContainer;
     private static final double DEFAULT_LATITUDE = 46.009628;
@@ -34,55 +41,52 @@ public class LocalitysirutaDetailView extends StandardDetailView<Localitysiruta>
 
     //For GoogleMap addon from  https://github.com/FlowingCode/GoogleMapsAddon
     private GoogleMap gmaps;
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        initGoogleMap();
-        addGoogleMapToContainer();
+    // For leaflet with OpenStreetMapMap from https://github.com/xdev-software/vaadin-maps-leaflet-flow
+    private LMap map;
+    @Subscribe("checkbox")
+    public void onCheckboxClick(final ClickEvent<Checkbox> event) {
+        String message_1 = messageBundle.getMessage("google_map");
+        String message_2 = messageBundle.getMessage("openstreetmap");
+        if (Boolean.TRUE.equals(event.getSource().getValue())) {
+            notifications.create(message_1).show();
+            initGoogleMap();
+            remMapFromContainer();
+            addGoogleMapToContainer();
+            addGoogleMapMarker();
+        } else {
+            notifications.create(message_2).show();
+            initMap();
+            remGoogleMapFromContainer();
+            addMapToContainer();
+            addMapMarker();
+        }
     }
     @Subscribe
     protected void onBeforeShow(BeforeShowEvent event) {
+        if (checkbox.getValue()) {
+            initGoogleMap();
+            addGoogleMapToContainer();
+            addGoogleMapMarker();
+        }
+        else {
+            initMap();
+            addMapToContainer();
+            addMapMarker();
+        }
+    }
+    private void addGoogleMapMarker() {
         Localitysiruta localitysiruta = getEditedEntity();
         if (localitysiruta.getLatitude() != null && localitysiruta.getLongitude() != null) {
             GoogleMapMarker localityMarker = gmaps.addMarker(localitysiruta.getName(),
                     new LatLon(localitysiruta.getLatitude(), localitysiruta.getLongitude()),
                     false, Markers.LIGHTBLUE_DOT);
-           String locality = localitysiruta.getName();
-           localityMarker.addInfoWindow("<h2>"+locality+"</h2>");
+            String locality = localitysiruta.getName();
+            localityMarker.addInfoWindow("<h2>"+locality+"</h2>");
         }
         else drawGoogleCenterMarkers();
     }
-    private void initGoogleMap() {
-        String apiKey = "";//add your api key from Google Map
-        gmaps = new GoogleMap(apiKey, null, null);
-        gmaps.setMapType(GoogleMap.MapType.ROADMAP);
-        gmaps.setCenter(new LatLon(DEFAULT_LATITUDE, DEFAULT_LONGITUDE));
-        gmaps.setZoom(ZOOM_LEVEL);
-        gmaps.setSizeFull();
-    }
-    private void addGoogleMapToContainer() {
-        mapContainer.add(gmaps);
-    }
-    private void drawGoogleCenterMarkers() {
-        GoogleMapMarker centerMarker = gmaps.addMarker("Centru", gmaps.getCenter(), true, Markers.ORANGE_DOT);
-        centerMarker.addInfoWindow("<h2>Mută-mă &#128540;</h2>");
-        centerMarker.addDragEndEventListener(event ->  {
-           getEditedEntity().setLongitude(event.getLongitude());
-           getEditedEntity().setLatitude(event.getLatitude());
-       });
-    }
-
-    // For leaflet with OpenStreetMapMap from https://github.com/xdev-software/vaadin-maps-leaflet-flow
-/*
-    private LMap map;
-    @Subscribe
-    protected void onInit(InitEvent event) {
-        initMap();
-        addMapToContainer();
-    }
-    @Subscribe
-    protected void onBeforeShow(BeforeShowEvent event) {
+    private void addMapMarker() {
         Localitysiruta localitysiruta = getEditedEntity();
-
         if (localitysiruta.getLatitude() != null && localitysiruta.getLongitude() != null) {
             LMarker localityMarker = new LMarker(localitysiruta.getLatitude(), localitysiruta.getLongitude());
             localityMarker.setPopup(localitysiruta.getName());
@@ -100,10 +104,37 @@ public class LocalitysirutaDetailView extends StandardDetailView<Localitysiruta>
     private void addMapToContainer() {
         mapContainer.add(map);
     }
+    private void remMapFromContainer() {
+        mapContainer.remove(map);
+    }
     private void drawCenterMarkers() {
+        String message_1 = messageBundle.getMessage("center");
         LMarker markerCenter = new LMarker(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
-        markerCenter.setPopup("Punctul de centrare");
+        markerCenter.setPopup(message_1);
         map.addLComponents(markerCenter);
     }
-*/
+    private void initGoogleMap() {
+        String apiKey = "";//add your api key from Google Map
+        gmaps = new GoogleMap(apiKey, null, null);
+        gmaps.setMapType(GoogleMap.MapType.ROADMAP);
+        gmaps.setCenter(new LatLon(DEFAULT_LATITUDE, DEFAULT_LONGITUDE));
+        gmaps.setZoom(ZOOM_LEVEL-1);//low zoom little for Google Maps
+        gmaps.setSizeFull();
+    }
+    private void addGoogleMapToContainer() {
+        mapContainer.add(gmaps);
+    }
+    private void remGoogleMapFromContainer() {
+        mapContainer.remove(gmaps);
+    }
+    private void drawGoogleCenterMarkers() {
+        String message_1 = messageBundle.getMessage("center");
+        String message_2 = messageBundle.getMessage("move_me");
+        GoogleMapMarker centerMarker = gmaps.addMarker(message_1, gmaps.getCenter(), true, Markers.ORANGE_DOT);
+        centerMarker.addInfoWindow("<h2>"+message_2+"</h2>");
+        centerMarker.addDragEndEventListener(event ->  {
+           getEditedEntity().setLongitude(event.getLongitude());
+           getEditedEntity().setLatitude(event.getLatitude());
+       });
+    }
 }
