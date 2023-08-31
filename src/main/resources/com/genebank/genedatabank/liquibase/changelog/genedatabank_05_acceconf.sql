87a5131dd49d51876cbdb9787bee57b8efc349cd
Florin Tanasa <florin.tanasa@gmail.com>
2023-08-31 10:51:59 +0300

    update: Added sql data for public.acceconf table

---
+++ src/main/resources/com/genebank/genedatabank/liquibase/changelog/genedatabank_05_acceconf.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.acceconf (id,"version",created_by,created_date,last_modified_by,last_modified_date,deleted_by,deleted_date,code,name) VALUES
+	 ('1a1c75e6-da84-f369-3fa9-3a401ed3f88e',1,'admin','2023-08-31 10:45:03.321+03',NULL,'2023-08-31 10:45:03.321+03',NULL,NULL,0,'Nu este confidențială'),
+	 ('4bafd7fb-ad15-74c3-2c51-0c4f796756bc',1,'admin','2023-08-31 10:45:14.811+03',NULL,'2023-08-31 10:45:14.811+03',NULL,NULL,1,'Este confidențială');
