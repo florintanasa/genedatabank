9f91b96df95650c96af7f623db9f098e046e8622
Florin Tanasa <florin.tanasa@gmail.com>
2023-09-06 11:41:53 +0300

    update: Added component to data grid to show qr code image

--- src/main/java/com/genebank/genedatabank/view/deposit/DepositListView.java
+++ src/main/java/com/genebank/genedatabank/view/deposit/DepositListView.java
@@ -4,8 +4,17 @@ import com.genebank.genedatabank.entity.Deposit;
 
 import com.genebank.genedatabank.view.main.MainView;
 
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import io.jmix.core.FileRef;
+import io.jmix.core.FileStorage;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
@@ -18,4 +27,34 @@ import io.jmix.flowui.view.*;
 @LookupComponent("depositsDataGrid")
 @DialogMode(width = "64em")
 public class DepositListView extends StandardListView<Deposit> {
+    @ViewComponent
+    private DataGrid<Deposit> depositsDataGrid;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private FileStorage fileStorage;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        Grid.Column<Deposit> qrcodeColumn = depositsDataGrid.addComponentColumn(deposit -> {
+            FileRef fileRef = deposit.getQrcode();
+            if (fileRef != null) {
+                Image image = uiComponents.create(Image.class);
+                image.setWidth("75px");
+                image.setHeight("75px");
+                StreamResource streamResource = new StreamResource(fileRef.getFileName(),
+                        () -> fileStorage.openStream(fileRef));
+                image.setSrc(streamResource);
+                image.setClassName("qrcode-image");
+
+                return image;
+            } else {
+                return new Span();
+            }
+        });
+        qrcodeColumn.setFlexGrow(0);
+        qrcodeColumn.setWidth("85px");
+        qrcodeColumn.setHeader("QR");
+        depositsDataGrid.setColumnPosition(qrcodeColumn, 12);
+    }
 }
