a1027bd6408d7b5a1059699888643e71b0286eb8
Florin Tanasa <florin.tanasa@gmail.ro>
2024-08-21 21:43:31 +0300

    update: Extend main application with SpringBootServletInitializer for war deploy

--- src/main/java/com/genebank/genedatabank/GenedatabankApplication.java
+++ src/main/java/com/genebank/genedatabank/GenedatabankApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.EventListener;
@@ -23,7 +24,7 @@ import javax.sql.DataSource;
 @Theme(value = "genedatabank")
 @PWA(name = "Genedatabank", shortName = "Genedatabank")
 @SpringBootApplication
-public class GenedatabankApplication implements AppShellConfigurator {
+public class GenedatabankApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
     @Autowired
     private Environment environment;
