6c9c0e58683279fce23009ace74b2e22ae6423e9
Florin Tanasa <florin.tanasa@gmail.com>
2024-09-09 11:31:45 +0300

    update: Added button for action Copy and edit a entity

--- src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
+++ src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
@@ -5,7 +5,14 @@ import com.genebank.genedatabank.entity.Pasaport;
 import com.genebank.genedatabank.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.core.MetadataTools;
+import io.jmix.core.UuidProvider;
+import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.view.*;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
@@ -18,4 +25,29 @@ import io.jmix.flowui.view.*;
 @LookupComponent("pasaportsDataGrid")
 @DialogMode(width = "64em")
 public class PasaportListView extends StandardListView<Pasaport> {
+    @ViewComponent
+    private DataGrid<Pasaport> pasaportsDataGrid;
+    @Autowired
+    private MetadataTools metadataTools;
+    @Autowired
+    private DialogWindows dialogWindows;
+
+    @Subscribe("pasaportsDataGrid.copyEdit")
+    public void onPasaportsDataGridCopyEdit(final ActionPerformedEvent event) {
+        Pasaport pasaport = pasaportsDataGrid.getSingleSelectedItem();
+        Pasaport pasaportCopy = createCopy(pasaport);
+        dialogWindows.detail(pasaportsDataGrid)
+                .withViewClass(PasaportDetailView.class)
+                .newEntity(pasaportCopy)
+                .open();
+        
+    }
+
+    public Pasaport createCopy(Pasaport pasaport) {
+        Pasaport pasaportCopy = metadataTools.copy(pasaport);
+        pasaportCopy.setId(UuidProvider.createUuid());
+        pasaportCopy.setAccenumb(null);
+        pasaportCopy.setAccname(StringUtils.abbreviate("Copie a " + pasaport.getAccname(),45));
+        return pasaportCopy;
+    }
 }
