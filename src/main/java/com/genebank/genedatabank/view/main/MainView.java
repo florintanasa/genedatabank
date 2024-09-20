b75dc8a6a8556566b47c882ae20f4aa41dfc3990
Florin Tanasa <florin.tanasa@gmail.com>
2024-09-20 19:18:46 +0300

    update: Added button to change the theme from Light to Dark

--- src/main/java/com/genebank/genedatabank/view/main/MainView.java
+++ src/main/java/com/genebank/genedatabank/view/main/MainView.java
@@ -1,12 +1,54 @@
 package com.genebank.genedatabank.view.main;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.app.main.StandardMainView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("")
 @ViewController("MainView")
 @ViewDescriptor("main-view.xml")
 public class MainView extends StandardMainView {
+    @Autowired
+    private UiComponents uiComponents;
+    @ViewComponent
+    private MessageBundle messageBundle;
+    @ViewComponent
+    private Header header;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        initManualThemeChange();
+    }
+    
+    protected void initManualThemeChange() {
+        JmixButton themeChangeButton = createThemeButton();
+        themeChangeButton.addClickListener(e -> {
+            ThemeList themeList = UI.getCurrent().getElement().getThemeList();
+            if (themeList.contains(Lumo.DARK)) {
+                themeList.remove(Lumo.DARK);
+            } else {
+                themeList.add(Lumo.DARK);
+            }
+        });
+        header.addComponentAsFirst(themeChangeButton);
+    }
+
+    protected JmixButton createThemeButton() {
+        JmixButton themeButton = uiComponents.create(JmixButton.class);
+        themeButton.setIcon(VaadinIcon.CIRCLE.create());
+        themeButton.addThemeVariants(ButtonVariant.LUMO_ICON, ButtonVariant.LUMO_TERTIARY_INLINE);
+        String toolTipThemeButton = messageBundle.getMessage("toolTipThemeButtonMessage");
+        themeButton.setTooltipText(toolTipThemeButton);
+
+        return themeButton;
+    }
 }
