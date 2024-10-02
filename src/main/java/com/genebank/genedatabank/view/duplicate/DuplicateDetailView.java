da4d5ddcde0cafde6fc094ed678295d6f6123ac4
Florin Tanasa <florin.tanasa@svgenebank.ro>
2024-10-02 10:12:02 +0300

    update: Added validation check. Save only is possible to the user.

--- src/main/java/com/genebank/genedatabank/view/duplicate/DuplicateDetailView.java
+++ src/main/java/com/genebank/genedatabank/view/duplicate/DuplicateDetailView.java
@@ -3,12 +3,17 @@ package com.genebank.genedatabank.view.duplicate;
 import com.genebank.genedatabank.entity.Duplicate;
 import com.genebank.genedatabank.entity.DuplicateStatus;
 import com.genebank.genedatabank.entity.Institute;
+import com.genebank.genedatabank.entity.User;
 import com.genebank.genedatabank.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.TimeSource;
-import io.jmix.flowui.component.valuepicker.EntityPicker;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.combobox.EntityComboBox;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 @Route(value = "duplicates/:id", layout = MainView.class)
 @ViewController("Duplicate.detail")
@@ -18,7 +23,15 @@ public class DuplicateDetailView extends StandardDetailView<Duplicate> {
     @Autowired
     private TimeSource timeSource;
     @ViewComponent
-    private EntityPicker<Institute> id_duplicate_instituteField;
+    private EntityComboBox<Institute> id_duplicate_instituteComboBox;
+    @ViewComponent
+    private EntityComboBox<Institute> id_send_instituteComboBox;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+    @Autowired
+    private MessageBundle messageBundle;
+    @Autowired
+    private Notifications notifications;
 
     @Subscribe
     public void onInitEntity(final InitEntityEvent<Duplicate> event) {
@@ -26,9 +39,21 @@ public class DuplicateDetailView extends StandardDetailView<Duplicate> {
         event.getEntity().setStatus(DuplicateStatus.PREPARED);
         //set default the date to now
         event.getEntity().setTheDate(timeSource.now().toLocalDate());
-
         //focus to Duplicate Institute field for choose
-        id_duplicate_instituteField.focus();
+        id_duplicate_instituteComboBox.focus();
     }
+
+    // check what user try to save, if is not from the same institute him can't save
+    @Subscribe
+    public void onValidation(final ValidationEvent event) {
+        final User user = (User) currentAuthentication.getUser();
+        if (!Objects.equals(getEditedEntity().getId_send_institute(), user.getId_institute())) {
+            String can_not_insert_for_over_institute = messageBundle.getMessage("can_not_insert_for_over_institute");
+            event.getErrors().add(messageBundle.getMessage("can_not_insert_for_over_institute"));
+            notifications.create("HOPA", can_not_insert_for_over_institute).withDuration(5000).show();
+        }
+    }
+
+    
     
 }
