ca99c4b67dd82cff1409528f6a9e1a2d5726bfba
Florin Tanasa <florin.tanasa@gmail.com>
2024-10-05 20:23:54 +0300

    update: Invalidate create action for all user and set access to edit action only to admin user

--- src/main/java/com/genebank/genedatabank/view/duplicateline/DuplicateLineListView.java
+++ src/main/java/com/genebank/genedatabank/view/duplicateline/DuplicateLineListView.java
@@ -1,9 +1,18 @@
 package com.genebank.genedatabank.view.duplicateline;
 
 import com.genebank.genedatabank.entity.DuplicateLine;
+import com.genebank.genedatabank.entity.User;
 import com.genebank.genedatabank.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.flowui.action.list.CreateAction;
+import io.jmix.flowui.action.list.EditAction;
+import io.jmix.flowui.action.list.ReadAction;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 
 @Route(value = "duplicateLines", layout = MainView.class)
@@ -12,4 +21,25 @@ import io.jmix.flowui.view.*;
 @LookupComponent("duplicateLinesDataGrid")
 @DialogMode(width = "64em")
 public class DuplicateLineListView extends StandardListView<DuplicateLine> {
+    @ViewComponent("duplicateLinesDataGrid.create")
+    private CreateAction<DuplicateLine> duplicateLinesDataGridCreate;
+    @ViewComponent
+    private JmixButton editBtn;
+    @ViewComponent("duplicateLinesDataGrid.read")
+    private ReadAction<DuplicateLine> duplicateLinesDataGridRead;
+    @ViewComponent("duplicateLinesDataGrid.edit")
+    private EditAction<DuplicateLine> duplicateLinesDataGridEdit;
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        final User user = (User) currentAuthentication.getUser();
+        if (!Objects.equals(user.getUsername(), "admin")) {
+            duplicateLinesDataGridEdit.setEnabled(false);
+        }
+        duplicateLinesDataGridCreate.setEnabled(false);
+
+    }
+    
 }
