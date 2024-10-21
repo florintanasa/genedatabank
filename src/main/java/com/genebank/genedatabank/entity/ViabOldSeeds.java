116f18138fe5e07c8f71d0a275bbe7213c22f8ae
Florin Tanasa <florin.tanasa@svgenebank.ro>
2024-10-21 13:42:12 +0300

    update: Create entities ViabOldSeeds and ViabOldSeedsLine

---
+++ src/main/java/com/genebank/genedatabank/entity/ViabOldSeeds.java
@@ -0,0 +1,231 @@
+/**
+ * @author : Florin Tanasă
+ * @since : 21.10.2024
+ **/
+
+package com.genebank.genedatabank.entity;
+
+import io.jmix.core.DeletePolicy;
+import io.jmix.core.annotation.DeletedBy;
+import io.jmix.core.annotation.DeletedDate;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.OnDelete;
+import io.jmix.core.metamodel.annotation.Composition;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "VIAB_OLD_SEEDS", indexes = {
+        @Index(name = "IDX_VIAB_OLD_SEEDS_ID_DEPOSIT_CODE", columnList = "ID_DEPOSIT_CODE_ID")
+})
+@Entity
+public class ViabOldSeeds {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    private OffsetDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    private OffsetDateTime lastModifiedDate;
+
+    @DeletedBy
+    @Column(name = "DELETED_BY")
+    private String deletedBy;
+
+    @DeletedDate
+    @Column(name = "DELETED_DATE")
+    private OffsetDateTime deletedDate;
+
+    @Column(name = "ID_VOS", nullable = false, length = 15)
+    @NotNull
+    private String idVOS;
+
+    @JoinColumn(name = "ID_DEPOSIT_CODE_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Deposit id_deposit_code;
+
+    @Column(name = "STOCK")
+    private Integer stock;
+
+    @Column(name = "LAST_YEAR_TEST")
+    private Integer lastYearTest;
+
+    @Column(name = "LAST_VIAB_TEST")
+    private Integer lastViabTest;
+
+    @Column(name = "YEAR_TEST")
+    private Integer yearTest;
+
+    @Column(name = "VIAB_PERCENT")
+    private Integer viabPercent;
+
+    @Column(name = "STATUS")
+    private String status;
+
+    @OnDelete(DeletePolicy.CASCADE)
+    @Composition
+    @OneToMany(mappedBy = "viabOldSeeds")
+    private List<ViabOldSeedsLine> viaboldseedsLines;
+
+    public List<ViabOldSeedsLine> getViaboldseedsLines() {
+        return viaboldseedsLines;
+    }
+
+    public void setViaboldseedsLines(List<ViabOldSeedsLine> viaboldseedsLines) {
+        this.viaboldseedsLines = viaboldseedsLines;
+    }
+
+    public ViabilityStatus getStatus() {
+        return status == null ? null : ViabilityStatus.fromId(status);
+    }
+
+    public void setStatus(ViabilityStatus status) {
+        this.status = status == null ? null : status.getId();
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getViabPercent() {
+        return viabPercent;
+    }
+
+    public void setViabPercent(Integer viabPercent) {
+        this.viabPercent = viabPercent;
+    }
+
+    public Integer getYearTest() {
+        return yearTest;
+    }
+
+    public void setYearTest(Integer yearTest) {
+        this.yearTest = yearTest;
+    }
+
+    public Integer getLastViabTest() {
+        return lastViabTest;
+    }
+
+    public void setLastViabTest(Integer lastViabTest) {
+        this.lastViabTest = lastViabTest;
+    }
+
+    public Integer getLastYearTest() {
+        return lastYearTest;
+    }
+
+    public void setLastYearTest(Integer lastYearTest) {
+        this.lastYearTest = lastYearTest;
+    }
+
+    public Deposit getId_deposit_code() {
+        return id_deposit_code;
+    }
+
+    public void setId_deposit_code(Deposit id_deposit_code) {
+        this.id_deposit_code = id_deposit_code;
+    }
+
+    public String getIdVOS() {
+        return idVOS;
+    }
+
+    public void setIdVOS(String idVOS) {
+        this.idVOS = idVOS;
+    }
+
+    public OffsetDateTime getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(OffsetDateTime deletedDate) {
+        this.deletedDate = deletedDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public OffsetDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(OffsetDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public OffsetDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(OffsetDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/genebank/genedatabank/entity/ViabOldSeedsLine.java
@@ -0,0 +1,238 @@
+/**
+ * @author : Florin Tanasă
+ * @since : 21.10.2024
+ **/
+
+package com.genebank.genedatabank.entity;
+
+import io.jmix.core.DeletePolicy;
+import io.jmix.core.annotation.DeletedBy;
+import io.jmix.core.annotation.DeletedDate;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.OnDeleteInverse;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "VIAB_OLD_SEEDS_LINE", indexes = {
+        @Index(name = "IDX_VIAB_OLD_SEEDS_LINE_VIAB_OLD_SEEDS", columnList = "VIAB_OLD_SEEDS_ID")
+})
+@Entity
+public class ViabOldSeedsLine {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    private OffsetDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    private OffsetDateTime lastModifiedDate;
+
+    @DeletedBy
+    @Column(name = "DELETED_BY")
+    private String deletedBy;
+
+    @DeletedDate
+    @Column(name = "DELETED_DATE")
+    private OffsetDateTime deletedDate;
+
+    @OnDeleteInverse(DeletePolicy.CASCADE)
+    @JoinColumn(name = "VIAB_OLD_SEEDS_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private ViabOldSeeds viabOldSeeds;
+
+    @Positive(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.seedsNum.validation.Positive}")
+    @Column(name = "SEEDS_NUM")
+    private Integer seedsNum;
+
+    @Column(name = "GERM_START_DATE")
+    private LocalDate germStartDate;
+
+    @PositiveOrZero(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.germTime.validation.PositiveOrZero}")
+    @Column(name = "GERM_TIME")
+    private Integer germTime;
+
+    @PositiveOrZero(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.treatTime.validation.PositiveOrZero}")
+    @Column(name = "TREAT_TIME")
+    private Integer treatTime;
+
+    @Column(name = "GERM_EVAL_DATE")
+    private LocalDate germEvalDate;
+
+    @PositiveOrZero(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.viableSeeds.validation.PositiveOrZero}")
+    @Column(name = "VIABLE_SEEDS")
+    private Integer viableSeeds;
+
+    @PositiveOrZero(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.germFaculty.validation.PositiveOrZero}")
+    @Max(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.germFaculty.validation.Max}", value = 100)
+    @Min(message = "{msg://com.genebank.genedatabank.entity/ViabNewSeedsLine.germFaculty.validation.Min}", value = 0)
+    @Column(name = "GERM_FACULTY")
+    private Integer germFaculty;
+
+    @Column(name = "COMMENTS")
+    private String comments;
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Integer getGermFaculty() {
+        return germFaculty;
+    }
+
+    public void setGermFaculty(Integer germFaculty) {
+        this.germFaculty = germFaculty;
+    }
+
+    public Integer getViableSeeds() {
+        return viableSeeds;
+    }
+
+    public void setViableSeeds(Integer viableSeeds) {
+        this.viableSeeds = viableSeeds;
+    }
+
+    public LocalDate getGermEvalDate() {
+        return germEvalDate;
+    }
+
+    public void setGermEvalDate(LocalDate germEvalDate) {
+        this.germEvalDate = germEvalDate;
+    }
+
+    public Integer getTreatTime() {
+        return treatTime;
+    }
+
+    public void setTreatTime(Integer treatTime) {
+        this.treatTime = treatTime;
+    }
+
+    public Integer getGermTime() {
+        return germTime;
+    }
+
+    public void setGermTime(Integer germTime) {
+        this.germTime = germTime;
+    }
+
+    public LocalDate getGermStartDate() {
+        return germStartDate;
+    }
+
+    public void setGermStartDate(LocalDate germStartDate) {
+        this.germStartDate = germStartDate;
+    }
+
+    public Integer getSeedsNum() {
+        return seedsNum;
+    }
+
+    public void setSeedsNum(Integer seedsNum) {
+        this.seedsNum = seedsNum;
+    }
+
+    public ViabOldSeeds getViabOldSeeds() {
+        return viabOldSeeds;
+    }
+
+    public void setViabOldSeeds(ViabOldSeeds viabOldSeeds) {
+        this.viabOldSeeds = viabOldSeeds;
+    }
+
+    public OffsetDateTime getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(OffsetDateTime deletedDate) {
+        this.deletedDate = deletedDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public OffsetDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(OffsetDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public OffsetDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(OffsetDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
