250c3d6fbfef78c4ab6cc8af97114263ed89deab
Florin Tanasa <florin.tanasa@gmail.com>
2024-11-04 20:01:01 +0200

    update: Change data from sql and order for files because after some modification give errors because some fields was deleted and also some was added

--- src/main/resources/com/genebank/genedatabank/liquibase/changelog/genedatabank_16_street.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.street (id,"version",created_by,created_date,last_modified_by,last_modified_date,deleted_by,deleted_date,name_street) VALUES
-	 ('378df788-90d5-a96b-c228-b5cba3840dd6',1,'admin','2023-09-01 11:08:25.487+03',NULL,'2023-09-01 11:08:25.487+03',NULL,NULL,'Ștefan cel Mare');
