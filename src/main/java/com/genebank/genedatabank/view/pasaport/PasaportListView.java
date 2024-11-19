5d4c60ecb02250243d6fb5f0e46332d03a74091c
Florin Tanasa <florin.tanasa@gmail.com>
2024-11-19 18:30:44 +0200

    update: Rewritten function to copy Pasaport entity.

--- src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
+++ src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
@@ -5,6 +5,7 @@ import com.genebank.genedatabank.entity.Pasaport;
 import com.genebank.genedatabank.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
 import io.jmix.core.MetadataTools;
 import io.jmix.core.UuidProvider;
 import io.jmix.flowui.DialogWindows;
@@ -25,6 +26,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 @LookupComponent("pasaportsDataGrid")
 @DialogMode(width = "64em")
 public class PasaportListView extends StandardListView<Pasaport> {
+    @Autowired
+    protected DataManager dataManager;
     @ViewComponent
     private DataGrid<Pasaport> pasaportsDataGrid;
     @Autowired
@@ -36,6 +39,8 @@ public class PasaportListView extends StandardListView<Pasaport> {
     public void onPasaportsDataGridCopyEdit(final ActionPerformedEvent event) {
         Pasaport pasaport = pasaportsDataGrid.getSingleSelectedItem();
         Pasaport pasaportCopy = createCopy(pasaport);
+        pasaportCopy.setAccenumb(null);
+        //pasaportCopy.setId(UuidProvider.createUuid());
         dialogWindows.detail(pasaportsDataGrid)
                 .withViewClass(PasaportDetailView.class)
                 .newEntity(pasaportCopy)
@@ -44,10 +49,47 @@ public class PasaportListView extends StandardListView<Pasaport> {
     }
 
     public Pasaport createCopy(Pasaport pasaport) {
-        Pasaport pasaportCopy = metadataTools.copy(pasaport);
-        pasaportCopy.setId(UuidProvider.createUuid());
-        pasaportCopy.setAccenumb(null);
-        pasaportCopy.setAccname(StringUtils.abbreviate("Copie a " + pasaport.getAccname(),45));
-        return pasaportCopy;
+        //Pasaport pasaportCopy = metadataTools.copy(pasaport);
+        //Pasaport pasaportCopy = dataManager.load(Pasaport.class).id(pasaport.getId()).one();
+        //pasaportCopy.setId(UuidProvider.createUuid());
+        //pasaportCopy.setAccenumb(null);
+        //pasaportCopy.setAccname(StringUtils.abbreviate("Copie a " + pasaport.getAccname(),45));
+        //return pasaportCopy;
+        Pasaport pasaportNew = dataManager.create(Pasaport.class);
+        pasaportNew.setAccname(StringUtils.abbreviate("Copie a " + pasaport.getAccname(),45));
+        pasaportNew.setAcceurl(pasaport.getAcceurl());
+        pasaportNew.setAcqdate(pasaport.getAcqdate());
+        pasaportNew.setAncest(pasaport.getAncest());
+        pasaportNew.setColldate(pasaport.getColldate());
+        pasaportNew.setCollmissid(pasaport.getCollmissid());
+        pasaportNew.setCollnumb(pasaport.getCollnumb());
+        pasaportNew.setComments(pasaport.getComments());
+        pasaportNew.setDoi(pasaport.getDoi());
+        pasaportNew.setDonornumb(pasaport.getDonornumb());
+        pasaportNew.setElevation(pasaport.getElevation());
+        pasaportNew.setId_acceconf(pasaport.getId_acceconf());
+        pasaportNew.setId_aegisstat(pasaport.getId_aegisstat());
+        pasaportNew.setId_bredcode(pasaport.getId_bredcode());
+        pasaportNew.setId_collsrc(pasaport.getId_collsrc());
+        pasaportNew.setId_country(pasaport.getId_country());
+        pasaportNew.setId_collcode(pasaport.getId_collcode());
+        pasaportNew.setId_countysiruta(pasaport.getId_countysiruta());
+        pasaportNew.setId_donorcode(pasaport.getId_donorcode());
+        pasaportNew.setId_duplsite(pasaport.getId_duplsite());
+        pasaportNew.setId_georefmeth(pasaport.getId_georefmeth());
+        pasaportNew.setId_historic(pasaport.getId_historic());
+        pasaportNew.setId_instcode(pasaport.getId_instcode());
+        pasaportNew.setId_localitysiruta(pasaport.getId_localitysiruta());
+        pasaportNew.setId_mlsstat(pasaport.getId_mlsstat());
+        pasaportNew.setId_sampstat(pasaport.getId_sampstat());
+        pasaportNew.setId_taxonomy(pasaport.getId_taxonomy());
+        pasaportNew.setLatitude(pasaport.getLatitude());
+        pasaportNew.setLongitude(pasaport.getLongitude());
+        pasaportNew.setOrigdate(pasaport.getOrigdate());
+        pasaportNew.setOthernumb(pasaport.getOthernumb());
+        pasaportNew.setProbeImages(pasaport.getProbeImages());
+        pasaportNew.setRemarks(pasaport.getRemarks());
+        pasaportNew.setTempnumb(pasaport.getTempnumb());
+        return pasaportNew;
     }
 }
