609757103ceec627e153a21c2031d9e0e6cd0e71
Florin Tanasa <florin.tanasa@svgenebank.ro>
2024-11-27 15:50:16 +0200

    update: Added method for copy and modify

--- src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
+++ src/main/java/com/genebank/genedatabank/view/pasaport/PasaportListView.java
@@ -4,10 +4,20 @@ import com.genebank.genedatabank.entity.Pasaport;
 
 import com.genebank.genedatabank.view.main.MainView;
 
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.flowui.Dialogs;
+import io.jmix.flowui.Notifications;
 import io.jmix.flowui.ViewNavigators;
+import io.jmix.flowui.app.inputdialog.DialogActions;
+import io.jmix.flowui.app.inputdialog.DialogOutcome;
+import io.jmix.flowui.app.inputdialog.InputParameter;
 import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.component.validation.ValidationErrors;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +37,12 @@ public class PasaportListView extends StandardListView<Pasaport> {
     private DataGrid<Pasaport> pasaportsDataGrid;
     @Autowired
     private ViewNavigators viewNavigators;
+    @Autowired
+    private Dialogs dialogs;
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private DataManager dataManager;
 
 
     @Subscribe("pasaportsDataGrid.copyEdit")
@@ -114,5 +130,119 @@ public class PasaportListView extends StandardListView<Pasaport> {
                             .setTempnumb(pasaport.getTempnumb());
                 })
                 .navigate();
+    }
+
+    //method for copy and modify with selected item other entity
+    public void navigateToCopyAndModifyEntity(Pasaport pasaport, Pasaport pasaport2) {
+        viewNavigators.detailView(pasaportsDataGrid)
+                .withViewClass(PasaportDetailView.class)
+                .editEntity(pasaport2)
+                .withAfterNavigationHandler(afterNavigationEvent -> {
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setAccname(StringUtils.abbreviate("Copie a "
+                                    + pasaport.getAccname(),45));
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setAcceurl(pasaport.getAcceurl());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setAcqdate(pasaport.getAcqdate());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setAncest(pasaport.getAncest());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setColldate(pasaport.getColldate());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setCollmissid(pasaport.getCollmissid());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setCollnumb(pasaport.getCollnumb());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setComments(pasaport.getComments());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setDoi(pasaport.getDoi());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setDonornumb(pasaport.getDonornumb());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setElevation(pasaport.getElevation());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_acceconf(pasaport.getId_acceconf());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_aegisstat(pasaport.getId_aegisstat());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_bredcode(pasaport.getId_bredcode());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_collsrc(pasaport.getId_collsrc());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_country(pasaport.getId_country());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_collcode(pasaport.getId_collcode());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_countysiruta(pasaport.getId_countysiruta());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_donorcode(pasaport.getId_donorcode());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_duplsite(pasaport.getId_duplsite());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_georefmeth(pasaport.getId_georefmeth());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_historic(pasaport.getId_historic());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_instcode(pasaport.getId_instcode());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_localitysiruta(pasaport.getId_localitysiruta());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_mlsstat(pasaport.getId_mlsstat());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_sampstat(pasaport.getId_sampstat());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setId_taxonomy(pasaport.getId_taxonomy());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setLatitude(pasaport.getLatitude());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setLongitude(pasaport.getLongitude());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setOrigdate(pasaport.getOrigdate());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setOthernumb(pasaport.getOthernumb());
+                    // afterNavigationEvent.getView().getEditedEntity()
+                    // .setProbeImages(pasaport.getProbeImages());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setRemarks(pasaport.getRemarks());
+                    afterNavigationEvent.getView().getEditedEntity()
+                            .setTempnumb(pasaport.getTempnumb());
+                })
+                .navigate();
+    }
+
+    @Subscribe(id = "copymodifBtn", subject = "clickListener")
+    public void onCopymodifBtnClick(final ClickEvent<JmixButton> event) {
+        dialogs.createInputDialog(this)
+                .withHeader("Modifică proba")
+                .withParameters(InputParameter.stringParameter("accenumber")
+                        .withLabel("Nr. de intrare")
+                )
+                .withValidator(validationContext -> {
+                    String accenumber = validationContext.getValue("accenumber");
+                    if (Strings.isNullOrEmpty(accenumber)) {
+                        return ValidationErrors.of("Nu ați specificat numărul de intrare!");
+                    }
+                    return ValidationErrors.none();
+                })
+                .withActions(DialogActions.OK_CANCEL)
+                .withCloseListener(inputDialogCloseEvent -> {
+                    if (inputDialogCloseEvent.closedWith(DialogOutcome.OK)) {
+                        //to do some actions
+                        String accenumber = inputDialogCloseEvent.getValue("accenumber");
+                        //notifications.create("Salut").withDuration(5000).show();
+                        Pasaport pasaport = pasaportsDataGrid.getSingleSelectedItem();
+                        Pasaport pasaport2 = dataManager.load(Pasaport.class)
+                                        .query("select p from Pasaport p " +
+                                                "where p.accenumb = :accenum")
+                                .parameter("accenum", accenumber)
+                                .one();
+                        //notifications.create(pasaport2.getDoi()).withDuration(5000).show();
+                        if (pasaport != null) {
+                            navigateToCopyAndModifyEntity(pasaport, pasaport2);
+                        }
+                    }
+                })
+                .open();
     }
 }
