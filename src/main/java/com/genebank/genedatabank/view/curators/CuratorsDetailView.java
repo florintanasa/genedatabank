2c7e0ccd090575ef5ee9086dbd0f5344c57bd8ee
Florin Tanasa <florin.tanasa@svgenebank.ro>
2025-06-02 11:11:27 +0300

    update: Complete with First and Last Name for the user used

--- src/main/java/com/genebank/genedatabank/view/curators/CuratorsDetailView.java
+++ src/main/java/com/genebank/genedatabank/view/curators/CuratorsDetailView.java
@@ -2,9 +2,13 @@ package com.genebank.genedatabank.view.curators;
 
 import com.genebank.genedatabank.entity.Curators;
 
+import com.genebank.genedatabank.entity.User;
 import com.genebank.genedatabank.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.component.checkbox.JmixCheckbox;
+import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.component.valuepicker.EntityPicker;
 import io.jmix.flowui.view.*;
 
 
@@ -17,4 +21,27 @@ import io.jmix.flowui.view.*;
 @ViewDescriptor("curators-detail-view.xml")
 @EditedEntityContainer("curatorsDc")
 public class CuratorsDetailView extends StandardDetailView<Curators> {
+    @ViewComponent
+    private EntityPicker<User> id_userField;
+    @ViewComponent
+    private TypedTextField<String> curator_nameField;
+    @ViewComponent
+    private JmixCheckbox activeField;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        //for check box Active now? to can be leave unchecked for false, problems after 2.5.0 jmix
+        activeField.setRequired(false);
+    }
+
+    @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        id_userField.addValueChangeListener(e -> {
+           if (e.getValue() != null) {
+               //if the user is selected or is changed add First and Last Name to the curator name
+               curator_nameField.setValue(e.getValue().getFirstName()+" "+e.getValue().getLastName());
+           }
+        });
+    }
+    
 }
